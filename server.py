a12d5d63661ed550a434957dd1e597d6bdbc5baa
Mohammed J Azad <87226195+MoJoAzad@users.noreply.github.com>
2021-10-13 20:36:15 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
-app=Flask(__name__)
+from flask import Flask,render_template
+app = Flask(__name__)
 
-@app.route("/play")
-def play():
-    return render_template('index.html',int=3)
+@app.route("/")
+def checkerboard():
+    return render_template('index.html')
 
-@app.route("/play/<int:number>")
-def mod(number):
-    return render_template('index.html',int=number)
+@app.route("/<int:number>")
+def number(number):
+    return render_template('index.html', int=number)
 
-@app.route("/play/<int:number>/<string:color>")
-def color(number, color):
-    return render_template('index.html',int=number, color=color)
+@app.route("/<int:number>/<string:color>")
+def colors(number, color):
+    return render_template('index.html', int=number, color=color)
 
 if __name__=="__main__":
     app.run(debug=True)
