19efa18f9021ca68f75602e90d0800be8d83905c
Mohammed J Azad <87226195+MoJoAzad@users.noreply.github.com>
2021-10-14 18:35:54 -0700

    for html table project

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route("/")
-def checkerboard():
-    return render_template('index.html')
+@app.route("/lists")
+def lists():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
 
-@app.route("/<int:number>")
-def number(number):
-    return render_template('index.html', int=number)
 
-@app.route("/<int:number>/<string:color>")
-def colors(number, color):
-    return render_template('index.html', int=number, color=color)
+    return render_template('index.html', users=users)
 
 if __name__=="__main__":
     app.run(debug=True)
