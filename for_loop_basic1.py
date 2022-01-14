c65c695d4cc4f44a24c3c45a8943e99351554ca6
Mohammed J Azad <87226195+MoJoAzad@users.noreply.github.com>
2022-01-14 13:45:32 -0800

    basic forloops

---
+++ for_loop_basic1.py
@@ -0,0 +1,24 @@
+for x in range(151):
+    print(x)
+
+for y in range(5, 1000, 5):
+    print(y)
+
+for z in range(0, 100):
+    print(z)
+    if z% 10 == 0:
+        print("Coding Dojo")
+    elif z% 5== 0:
+        print("Coding")
+
+sum = 0
+for a in range(1, 500000, 3):
+    sum+=a
+    print(sum)
+
+for b in range(2018, 0, -4):
+    print(b)
+
+for c in range(2,12):
+    if c%3==0:
+        print(c)
